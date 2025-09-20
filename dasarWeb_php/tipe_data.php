b11313f1d41bd891fb67d9f1f1ec66ac57ddf8d6
Arsya <arsyafikrisabilillah10@gmail.com>
2025-09-20 23:46:56 +0700

    praktikum 2

--- dasarWeb_php/tipe_data.php
+++ dasarWeb_php/tipe_data.php
@@ -22,5 +22,13 @@ echo "IPA: {$nilaiIpa} <br>";
 echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
 echo "Rata-rata: {$rataRata} <br>";
 var_dump($rataRata);
+echo "<br>";
+
+$apakahSiswaLulus = true;
+$apakahSiswaSudahUjian = false;
+
+var_dump($apakahSiswaLulus);
+echo "<br>";
+var_dump($apakahSiswaSudahUjian);
 
 ?>
