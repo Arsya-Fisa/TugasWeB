0e92cf2463dd9c56ca3d70904033cc6f13c23b17
Arsya <arsyafikrisabilillah10@gmail.com>
2025-09-21 12:15:29 +0700

    PRAKTIKUM 4

---
+++ dasarWeb_php/struktur_kontrol.php
@@ -0,0 +1,45 @@
+<?php
+    $nilaiNumerik = 92;
+    if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
+        echo "Nilai Huruf: A";
+    } elseif ($nilaiNumerik>= 80 && $nilaiNumerik <90) {
+        echo "Nilai Huruf: B";
+    } elseif ($nilaiNumerik >= 70 && $nilaiNumerik <80) {
+        echo "Nilai Huruf: C";
+    } elseif ($nilaiNumerik <70) {
+        echo "Nilai Huruf: D";
+    }
+    echo"<br>";
+
+$jarakSaatIni = 0;
+$jarakTarget = 500;
+$PeningkatanHarian = 30;
+$hari = 0;
+
+while ($jarakSaatIni < $jarakTarget) {
+    $jarakSaatIni += $PeningkatanHarian;
+    $hari++;
+}
+echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilomter";
+
+echo"<br>";
+$jumlahLahan = 10;
+$tanamanaPerlahan = 5;
+$buahPertanaman = 10;
+$jumlahBuah = 0;
+
+for ($i=1; $i <$jumlahLahan; $i++) { 
+    $jumlahBuah += ($tanamanaPerlahan * $buahPertanaman);
+}
+echo "Jumlah Buah yang akan dipanen adalah: $jumlahBuah";
+
+  echo"<br>";
+$skorUjian = [85, 92, 78, 96, 88];
+$totalSkor = 0;
+
+foreach ($skorUjian as $skor) {
+    $totalSkor += $skor;
+}
+echo "Total skor ujian adalah: $totalSkor";
+
+?>
