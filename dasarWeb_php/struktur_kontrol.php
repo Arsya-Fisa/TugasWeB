4c76181b5abad5a3cf3b0041936e8a28b46e7284
Arsya <arsyafikrisabilillah10@gmail.com>
2025-09-21 16:18:48 +0700

    praktikum 4

--- dasarWeb_php/struktur_kontrol.php
+++ dasarWeb_php/struktur_kontrol.php
@@ -42,4 +42,16 @@ foreach ($skorUjian as $skor) {
 }
 echo "Total skor ujian adalah: $totalSkor";
 
+ echo"<br>";
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+foreach($nilaiSiswa as $nilai) {
+    if ($nilai < 60) {
+        echo "Nilai: $nilai (tidak lulus) <br>";
+        continue;
+    }
+    echo "Nilai: $nilai (lulus) <br>";
+}
+
+
 ?>
