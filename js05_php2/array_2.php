eebc708e02fb4af7c2fb7e3b49fc50004a5621a5
Arsya <arsyafikrisabilillah10@gmail.com>
2025-09-27 20:59:12 +0700

    assosiative tabel

--- js05_php2/array_2.php
+++ js05_php2/array_2.php
@@ -3,6 +3,25 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title></title>
+        <style>
+            table {
+                border-collapse: collapse;
+                width: 350px;
+                margin: 20px 0;
+                font-family: Arial, sans-serif;
+            }
+            th, td {
+                border: 1px solid #888;
+                padding: 8px 12px;
+                text-align: left;
+            }
+            th {
+                background: #f2f2f2;
+            }
+            tr {
+                background: #85de8eff;
+            }
+        </style>
     </head>
     <body>
         <?php
@@ -11,9 +30,20 @@
                 'domisili'=> 'Malang',
                 'jenis_kelamin'=> 'Perempuan'
             ];
-            echo "Nama : {$Dosen ['nama']}<br>";
-            echo "Domisili: {$Dosen ['domisili']}<br>";
-            echo "Jenis Kelamin: {$Dosen ['jenis_kelamin']}<br>";
         ?>
+        <table>
+            <tr>
+                <th>Nama</th>
+                <td><?php echo $Dosen['nama']; ?></td>
+            </tr>
+            <tr>
+                <th>Domisili</th>
+                <td><?php echo $Dosen['domisili']; ?></td>
+            </tr>
+            <tr>
+                <th>Jenis Kelamin</th>
+                <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+            </tr>
+        </table>
     </body>
 </html>
