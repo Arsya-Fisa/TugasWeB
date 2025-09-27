b623fd9671119e9e4de21dd0a50da487dc659020
Arsya <arsyafikrisabilillah10@gmail.com>
2025-09-27 22:39:03 +0700

    tanggal

---
+++ js05_php2/date.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <body>
+        <h3> Date </h3>
+        <?php
+        echo "Today is " . date("Y/m/d") . "<br>";
+        echo "Today is " . date("Y.m.d") . "<br>";
+        echo "Today is " . date("Y-m-d") . "<br>";
+        echo "Today is " . date("l");
+        ?>
+        </body>
+    </head>
+</html>
