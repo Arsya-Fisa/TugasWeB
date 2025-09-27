8c87e255b70ee0aafa1bcb231e44e96127bf2f19
Arsya <arsyafikrisabilillah10@gmail.com>
2025-09-27 20:44:11 +0700

    fungsi array

---
+++ js05_php2/array_1.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <h2>Array Terindeks</h2>
+        <?php
+            $listDosen=["Elok Nur Hamdana","Unggul pamenang","Bagas Nugraha"];
+            echo $listDosen[2]. "<br>";
+            echo $listDosen[0]. "<br>";
+            echo $listDosen[1]. "<br>";
+        ?>
+    </body>
+</html>
