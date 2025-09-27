332018ef9f39b1688c85d46e8ff872a18d580121
Arsya <arsyafikrisabilillah10@gmail.com>
2025-09-27 19:58:49 +0700

    memanggil fungsi dalam fungsi

--- dasarWeb_php2/fungsi.php
+++ dasarWeb_php2/fungsi.php
@@ -19,6 +19,13 @@
         $umur = $tahun_sekarang - $tahun_lahir;
         return $umur;
     }
+    function perkenalkan($nama, $salam="Assalamualaikum") {
+        echo $salam.", ";
+        echo "Perkenalkan nama saya ".$nama."<br/>";
+        echo "Saya berusia ".hitungUmur(2005, 2025)." tahun<br/>";
+        echo "senang berkenalan dengan anda <br/>";
+    }
 
-    echo "Umur saya adalah ".hitungUmur(2005, 2025)." tahun";
+
+    perkenalkan("Arsya");
 ?>
