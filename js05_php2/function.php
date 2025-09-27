64a318c17780618f91a73a1527a4ff41c6636e6e
Arsya <arsyafikrisabilillah10@gmail.com>
2025-09-27 21:53:30 +0700

    fungsi parameter default

--- js05_php2/function.php
+++ js05_php2/function.php
@@ -1,5 +1,5 @@
 <?php
-    function perkenalan($nama, $salam) {
+    function perkenalan($nama, $salam="Assalamualaikum") {
         echo $salam.", ";
         echo "Perkenalkan, nama saya ".$nama."<br/>";
         echo "senang berkenalan dengan anda <br/>";
@@ -11,5 +11,5 @@
     $saya = "Elok";
     $ucapanSalam = "Selamat pagi";
 
-    perkenalan($saya, $ucapanSalam);
+    perkenalan($saya,);
 ?>
