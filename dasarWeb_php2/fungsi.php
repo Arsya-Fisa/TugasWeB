edad113e145c07e01f0179fd8b976a6c1c9e13a9
Arsya <arsyafikrisabilillah10@gmail.com>
2025-09-27 19:54:15 +0700

    fungsi mengembalikan  nilai

--- dasarWeb_php2/fungsi.php
+++ dasarWeb_php2/fungsi.php
@@ -12,4 +12,13 @@
     $ucapanSalam = "Selamat pagi";
 
     perkenalan($saya,);
+
+    echo "<br/>";
+    echo "<hr/>";
+    function hitungUmur($tahun_lahir, $tahun_sekarang) {
+        $umur = $tahun_sekarang - $tahun_lahir;
+        return $umur;
+    }
+
+    echo "Umur saya adalah ".hitungUmur(2005, 2025)." tahun";
 ?>
