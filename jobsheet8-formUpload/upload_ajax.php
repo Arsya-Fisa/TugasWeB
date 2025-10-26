f20f9ef9d7cb6ed60b2be3cdfefcf6f0a89b4150
Arsya <arsyafikrisabilillah10@gmail.com>
2025-10-26 20:01:16 +0700

    jobsheet 8

--- jobsheet8-formUpload/upload_ajax.php
+++ jobsheet8-formUpload/upload_ajax.php
@@ -1,31 +1,36 @@
 <?php
 if (isset($_FILES['file'])) {
     $errors = array();
-    $file_name = $_FILES['file']['name'];
-    $file_size = $_FILES['file']['size'];
-    $file_tmp = $_FILES['file']['tmp_name'];
-    $file_type = $_FILES['file']['type'];
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $totalFiles = count($_FILES['file']['name']);
 
-    $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+    for ($i = 0; $i < $totalFiles; $i++) {
+        $file_name = $_FILES['file']['name'][$i];
+        $file_size = $_FILES['file']['size'][$i];
+        $file_tmp = $_FILES['file']['tmp_name'][$i];
+        $file_type = $_FILES['file']['type'][$i];
+        $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
 
-    $extensions = array("pdf", "doc", "docx", "txt");
+        if (!in_array($file_ext, $allowedExtensions)) {
+            $errors[] = "File $file_name tidak valid. Hanya JPG, JPEG, PNG, dan GIF yang diperbolehkan.<br>";
+            continue;
+        }
 
+        if ($file_size > 2 * 1024 * 1024) {
+            $errors[] = "Ukuran file $file_name melebihi 2 MB.<br>";
+            continue;
+        }
 
-    if (in_array($file_ext, $extensions) === false) {
-        $errors[] = "Ekstensi file yang diizinkan adalah PDF, DOC, DOCX, atau TXT.";
+        if (move_uploaded_file($file_tmp, "uploads/" . $file_name)) {
+            echo "File <b>$file_name</b> berhasil diunggah.<br>";
+            echo "<img src='uploads/$file_name' width='150' style='height:auto; margin:5px; border:1px solid #ccc;'><br>";
+        } else {
+            $errors[] = "Gagal mengunggah file $file_name.<br>";
+        }
     }
 
-
-    if ($file_size > 2097152) { 
-        $errors[] = 'Ukuran file tidak boleh lebih dari 2 MB';
-    }
-
-
-    if (empty($errors) == true) {
-        move_uploaded_file($file_tmp, "documents/" . $file_name);
-        echo "File berhasil diunggah.";
-    } else {
-        echo implode(" ", $errors);
+    if (!empty($errors)) {
+        echo "<b>Daftar Error:</b><br>" . implode("", $errors);
     }
 }
 ?>
