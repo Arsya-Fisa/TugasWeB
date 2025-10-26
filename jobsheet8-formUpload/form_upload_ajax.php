bb12cb3796cdd26db440c658dc5b26edbf9edf34
Arsya <arsyafikrisabilillah10@gmail.com>
2025-10-26 20:08:39 +0700

    jobsheet 8

--- jobsheet8-formUpload/form_upload_ajax.php
+++ jobsheet8-formUpload/form_upload_ajax.php
@@ -1,14 +1,29 @@
+
 <html>
-    <head>
-        <title>Ungggah file Dokumen</title>
-    </head>
-    <body>
-        <form id= "upload-form"action="upload_ajax.php" method="POST"enctype="multipart/form-data">
-            <input type="file" name="file" id="file">
-            <input type="submit" name="submit" value="unggah">
-        </form>
-        <div id="status"></div>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="upload.js"></script>
-    </body>
+<head>
+<link rel="stylesheet" type="text/css" href="upload.css">
+<title>Unggah File Gambar</title>
+</head>
+<body>
+
+<div class="upload-form-container">
+    <h2>Unggah File Gambar</h2>
+>
+    <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
+
+        <div class="file-input-container">
+            <input type="file" name="file[]" id="file" class="file-input" multiple accept="image/*">
+            <label for="file" class="file-label">Pilih Gambar</label>
+        </div>
+
+        <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>Unggah</button>
+        
+        <div id="status" class="upload-status"></div>
+    </form>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="upload.js"></script>
+
+</body>
 </html>
