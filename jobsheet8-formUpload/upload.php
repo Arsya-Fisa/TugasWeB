e2c1f26758c5d464774441a33bec664aa2548cf3
Arsya <arsyafikrisabilillah10@gmail.com>
2025-10-26 19:28:15 +0700

    jobsheet8

---
+++ jobsheet8-formUpload/upload.php
@@ -0,0 +1,13 @@
+<?php
+if (isset($_POST["submit"])) {
+    $targetdir = "uploads/";
+    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+
+    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+        echo "FIle berhasil diunggah";
+    }
+    else {
+        echo "Gagal mengunggah file";
+    }
+}
+?>
