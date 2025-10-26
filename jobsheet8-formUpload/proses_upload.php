88f5e3933b2c316dec3188d6c107dcbd81441d59
Arsya <arsyafikrisabilillah10@gmail.com>
2025-10-26 19:46:32 +0700

    jobsheet 8

--- jobsheet8-formUpload/proses_upload.php
+++ jobsheet8-formUpload/proses_upload.php
@@ -1,24 +1,31 @@
 <?php
-$targetDirectory = "documents/";
+$targetDirectory = "uploads/"; // Folder tujuan upload
 
-
+// Buat folder jika belum ada
 if (!file_exists($targetDirectory)) {
     mkdir($targetDirectory, 0777, true);
 }
 
-if ($_FILES['files']['name'][0]) {
+// Pastikan ada file yang diunggah
+if (isset($_FILES['files']['name'][0])) {
     $totalFiles = count($_FILES['files']['name']);
-
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif"); // Hanya gambar
 
     for ($i = 0; $i < $totalFiles; $i++) {
         $fileName = $_FILES['files']['name'][$i];
-        $targetFile = $targetDirectory . $fileName;
+        $targetFile = $targetDirectory . basename($fileName);
+        $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-
-        if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
-            echo "File $fileName berhasil diunggah.<br>";
+        // Validasi ekstensi file
+        if (in_array($fileType, $allowedExtensions)) {
+            if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+                echo "File <b>$fileName</b> berhasil diunggah.<br>";
+                echo "<img src='$targetFile' width='150' style='height:auto; margin:5px; border:1px solid #ccc;'><br>";
+            } else {
+                echo "Gagal mengunggah file <b>$fileName</b>.<br>";
+            }
         } else {
-            echo "Gagal mengunggah file $fileName.<br>";
+            echo "File <b>$fileName</b> tidak valid (bukan gambar).<br>";
         }
     }
 } else {
