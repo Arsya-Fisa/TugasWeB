6f79f31afb04fe02e458639b03132158ec15add4
Arsya <arsyafikrisabilillah10@gmail.com>
2025-10-26 19:48:27 +0700

    josbheet 8

--- jobsheet8-formUpload/proses_upload.php
+++ jobsheet8-formUpload/proses_upload.php
@@ -1,12 +1,12 @@
 <?php
 $targetDirectory = "uploads/"; // Folder tujuan upload
 
-// Buat folder jika belum ada
+
 if (!file_exists($targetDirectory)) {
     mkdir($targetDirectory, 0777, true);
 }
 
-// Pastikan ada file yang diunggah
+
 if (isset($_FILES['files']['name'][0])) {
     $totalFiles = count($_FILES['files']['name']);
     $allowedExtensions = array("jpg", "jpeg", "png", "gif"); // Hanya gambar
@@ -16,7 +16,6 @@ if (isset($_FILES['files']['name'][0])) {
         $targetFile = $targetDirectory . basename($fileName);
         $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-        // Validasi ekstensi file
         if (in_array($fileType, $allowedExtensions)) {
             if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
                 echo "File <b>$fileName</b> berhasil diunggah.<br>";
