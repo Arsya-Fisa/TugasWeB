26ac7df7669a0842c9fdcc54738262dc743002a8
Arsya <arsyafikrisabilillah10@gmail.com>
2025-10-26 19:37:46 +0700

    jobsheet 8

--- jobsheet8-formUpload/upload.php
+++ jobsheet8-formUpload/upload.php
@@ -4,21 +4,22 @@ if (isset($_POST["submit"])) {
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    $allowedExstension = array("jpg", "jpeg", "png", "gif");
-    $maxsize = 5*1024*1024;
+    $allowedExtension = array("jpg", "jpeg", "png", "gif");
+    $maxsize = 5 * 1024 * 1024; // 5 MB
 
-    if (in_array($fileType, $allowedExstension) && $_FILES["myfile"]["size"]<= $maxsize)
-    {
+    if (in_array($fileType, $allowedExtension) && $_FILES["myfile"]["size"] <= $maxsize) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-            echo "FIle berhasil diunggah";
-        }
-        else {
-            echo "Gagal mengunggah file";
-        }
-    }
-    else {
-        echo " File tidak valid atau melebihi ukuran maxsimum yang diizinkan";
-    }
-    }
+            echo "File berhasil diunggah.<br>";
 
+
+            echo "<p>Preview Gambar:</p>";
+            echo "<img src='$targetfile' width='200'>";
+
+        } else {
+            echo "Gagal mengunggah file.";
+        }
+    } else {
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
+    }
+}
 ?>
